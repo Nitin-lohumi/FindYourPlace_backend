c51037473a4b8c410f4cd380e47148317079b2f1
Nitin-Lohumi <lohuminitin@gmail.com>
2025-07-11 16:11:04 +0530

    add

--- routes/nearBy.js
+++ routes/nearBy.js
@@ -1,7 +1,7 @@
 import express, { json } from "express";
 import SearchNearBy from "../services/SearchNearBy.js";
 import GetPlaceDetails from "../services/GetPlaceDetails.js";
-import FetchPhoto from "../services/GetPhoto.js";
+// import FetchPhoto from "../services/GetPhoto.js";
 import getSearchBarText from "../services/SearchBYText.js";
 import getRouteInfo from "../services/GetOtherDetails.js";
 const API_route = express.Router();
@@ -9,6 +9,7 @@ API_route.get("/searchNearBy", async (req, res) => {
     const { lat, long, type, radius } = req.query;
     try {
         const data = await SearchNearBy(lat, long, radius, type);
+        await wait(1000);
         res.status(200).json({ data: data, status: true });
     } catch (error) {
         console.error(" Error fetching places:", error.message);
@@ -20,7 +21,7 @@ API_route.get("/searchBar", async (req, res) => {
     try {
         const { text } = req.query;
         const data = await getSearchBarText(text);
-        // const data =text;
+        await wait(1000);
         res.status(200).json({ data: data, status: true });
     } catch (error) {
         res.status(500).json({ message: "Failed to fetch nearby places", status: false });
@@ -31,7 +32,7 @@ API_route.get("/placeDetails", async (req, res) => {
     try {
         const { placeid } = req.query;
         const data = await GetPlaceDetails(placeid);
-        console.log(data);
+        await wait(1000);
         return res.status(200).json({ data: data, status: true });
     } catch (error) {
         res.status(500).json({ message: "Failed to fetch nearby places", status: false });
@@ -42,6 +43,7 @@ API_route.get("/Searching_Type", async (req, res) => {
     try {
         const { type, lat, long } = req.query;
         const data = await SearchNearBy(lat, long, 5000, type);
+        await wait(1000);
         res.status(200).json({ data: data, status: true });
     } catch (error) {
         res.status(500).json({ message: "Failed to fetch nearby places", status: false });
@@ -55,6 +57,7 @@ API_route.get("/getRoute", async (req, res) => {
         const endlong = 79.458074;
         const endlat = 29.836903;
         const data = await getRouteInfo(startlng, startlat, endlong, endlat);
+        await wait(1000);
         res.status(200).json({ data: data, status: true });
     } catch (error) {
         console.error("Error fetching route:", error.message);
@@ -62,3 +65,7 @@ API_route.get("/getRoute", async (req, res) => {
     }
 });
 export default API_route;
+
+function wait(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
